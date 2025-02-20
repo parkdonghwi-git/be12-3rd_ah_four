e266ee021a8cd3b810a8da04493319280bdb4b48
kks <kks@example.com>
2025-02-20 15:51:32 +0900

    검색 요청 api는 search 모듈로
    병원관련된것들은 hospital 모듈로

--- hospital/src/main/java/com/example/hospital/model/Hospital.java
+++ hospital/src/main/java/com/example/hospital/model/Hospital.java
@@ -1,17 +1,10 @@
-package com.example.search.model;
+package com.example.hospital.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -28,25 +21,6 @@ public class Hospital {
     private String address;  // 주소
     @Column(nullable = false, length = 100)
     private String type;  // 진료 과목
-}
 
-    @Override
-    public String getUsername() {
-        return true;
-    }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
 }
--- search/src/main/java/com/example/search/SearchlController.java
+++ search/src/main/java/com/example/search/SearchlController.java
@@ -1,23 +1,23 @@
 package com.example.search;
 
-import com.example.search.model.Hospital;
-import com.example.search.model.HospitalService;
+import com.example.hospital.model.Hospital;
+import com.example.search.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/hospitals")
+@RequestMapping("/search")
 @RequiredArgsConstructor
 public class SearchlController {
 
-    private final HospitalService hospitalService;
+    private final SearchService searchService;
 
     // 모든 병원 조회
     @GetMapping
-    public List<Hospital> getAllHospitals() {
-        return hospitalService.getAllHospitals();
+    public List<Hospital> getAllSearch() {
+        return hospitalService.getAllSearch();
     }
 
     // 이름으로 병원 검색
